e3d7ff7e42e5233c86291050b34102faeabde01b
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-02-02 09:00:20 +0100

    Included dummy test app
    There is now a dummy app for when you want to test out the plugin, based
    on the debug component of the editor. The plugin card does not appear
    yet, as the editor content still needs to be set properly.

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,17 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+
+export default class ApplicationController extends Controller {
+  plugins = ['rdfa-editor-citaten-plugin'];
+
+  @action
+  rdfaEditorInit(controller) {
+    const presetContent = `<span resource="http://data.lblod.info/mappings/61DD4FF54402CC0009000002" typeof="ext:Mapping" data-editor-position-level="2" data-editor-rdfa-position-level="2">
+    <span property="dct:type" content="date"></span>
+    <span property="ext:content" datatype="xsd:date" data-editor-position-level="1" data-editor-rdfa-position-level="1">\${date}</span>
+  </span>`;
+    controller.setHtmlContent(presetContent);
+    const editorDone = new CustomEvent('editor-done');
+    window.dispatchEvent(editorDone);
+  }
+}
