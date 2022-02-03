94512139efe41920289a0e1b9997b17af1b4fc9c
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-02-03 11:46:23 +0100

    Allow Embroider builds to fail
    Set `allowedToFail: true` to allow the build to fail and return the
    errors, but not to stop the whole build of the app to fail, because
    Embroider is not a necessity yet.

--- config/ember-try.js
+++ config/ember-try.js
@@ -77,8 +77,8 @@ module.exports = async function () {
           },
         },
       },
-      embroiderSafe(),
-      embroiderOptimized(),
+      embroiderSafe({ allowedToFail: true }),
+      embroiderOptimized({ allowedToFail: true }),
     ],
   };
 };
