b3b8162954b1bbea775bd939137235a965795386
“[BenJay10]” <[benbehaeghe@gmail.com]>
2022-05-02 23:44:51 +0200

    Disable the button if out of context
    The button is disabled when the cursor in the editor is not inside the
    correct context for a reference. (This is instead of hiding the button.)

--- addon/components/editor-plugins/citaat-insert.js
+++ addon/components/editor-plugins/citaat-insert.js
@@ -4,12 +4,29 @@ import { tracked } from '@glimmer/tracking';
 import { LEGISLATION_TYPES } from '../../utils/legislation-types';
 
 export default class EditorPluginsCitaatInsertComponent extends Component {
+  @tracked disableInsert = false;
   @tracked showModal = false;
   @tracked legislationTypeUri = LEGISLATION_TYPES.decreet;
   @tracked text = '';
 
   constructor() {
     super(...arguments);
+    this.args.controller.onEvent(
+      'selectionChanged',
+      this.onSelectionChanged.bind(this)
+    );
+  }
+
+  onSelectionChanged() {
+    const limitedDatastore = this.args.controller.datastore.limitToRange(
+      this.args.controller.selection.lastRange,
+      'rangeIsInside'
+    );
+    const motivering = limitedDatastore
+      .match(null, '>http://data.vlaanderen.be/ns/besluit#motivering')
+      .asQuads()
+      .next().value;
+    this.disableInsert = motivering ? false : true;
   }
 
   @action
